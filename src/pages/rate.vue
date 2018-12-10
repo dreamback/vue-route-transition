23dedcfe03c2ae04697877462f19b3ee2ea5e25a
hesmmf@hehenian.com <hesmmf@hehenian.com>
2018-12-10 11:40:57 +0800

    demo

--- src/pages/rate.vue
+++ src/pages/rate.vue
@@ -9,7 +9,7 @@
     </div>
     <p style="text-align:center;padding-top:30px">请向下滚动</p>
     <van-cell-group style="margin:300px 0 300px">
-      <van-cell title="记录滚动条位置" is-link :to="{path:'/demo'}" />
+      <van-cell title="更多" is-link :to="{path:'/demo'}" />
     </van-cell-group>
   </router-layout>
 </template>
